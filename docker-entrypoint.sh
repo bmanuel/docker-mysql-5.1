ed720b6f75217fe19c83e41d80146968b35aa682
Jay Darnell <jay.darnell@gmail.com>
2019-03-28 11:15:09 -0400

    gz-support: add support for gz db backups

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -79,6 +79,7 @@ if [ "$1" = 'mysqld' ]; then
 			case "$f" in
 				*.sh)  echo "$0: running $f"; . "$f" ;;
 				*.sql) echo "$0: running $f"; "${mysql[@]}" < "$f" && echo ;;
+				*.sql.gz) echo "$0: running $f"; gunzip -c "$f" | "${mysql[@]}"; echo ;;
 				*)     echo "$0: ignoring $f" ;;
 			esac
 			echo
